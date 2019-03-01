c8d710f7e0320240d9a9dba1439bc0c4240d9d46
carlos <radix_10339@hotmail.com>
2019-03-01 15:57:38 -0600

    add hasher to password and read only fields

--- users/serializers.py
+++ users/serializers.py
@@ -9,3 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        read_only_fields = ('last_login', 'date_joined')
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
